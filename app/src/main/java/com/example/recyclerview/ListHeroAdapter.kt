66deeb1a79159e29451656f6bf39c43e8af6229b
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-12 20:41:14 +0700

    Successfuly add RecyclerView

---
+++ app/src/main/java/com/example/recyclerview/ListHeroAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapter (private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.recyclerview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvHeroes: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+    }
+}
